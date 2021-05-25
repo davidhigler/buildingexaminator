71d610f7dd770c3f6d86a5dcbf921ce6a070a37c
David C. Higler <davidhigler@gmail.com>
2021-05-25 21:39:13 +0200

    Add assets and changed the name of the api v1 documentation route.

--- src/Controller/Api/V1/ApiController.php
+++ src/Controller/Api/V1/ApiController.php
@@ -3,12 +3,12 @@
 namespace App\Controller\Api\V1;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api/v1/documentation', name: 'documentation', methods: ['GET'])]
+    #[Route('/api/v1/documentation', name: 'api-v1-documentation', methods: ['GET'])]
     public function Documentation(): Response
     {
         return new Response('Documentation');
