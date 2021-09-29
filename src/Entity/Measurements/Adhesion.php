cfa1671552e7a209e9a85c1808edbdfe137f0c0c
David C. Higler <davidhigler@gmail.com>
2021-09-29 19:35:44 +0200

    Added adhesion rating.

--- src/Entity/Measurements/Adhesion.php
+++ src/Entity/Measurements/Adhesion.php
@@ -3,13 +3,18 @@
 
 namespace App\Entity\Measurements;
 
-use App\Entity\Measurements\Ratings\AdhesionRating;
 use App\Entity\Portfolio\BuildingAddress;
 use App\Entity\SuperClasses\IdTime;
+
 
 class Adhesion extends IdTime
 {
     protected BuildingAddress $address;
 
-    protected AdhesionRating $adhesionRating;
+    protected int $adhesionRating;
+
+    public function __construct(int $adhesionRating)
+    {
+        $this->adhesionRating = $adhesionRating;
+    }
 }
