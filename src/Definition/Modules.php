27f1eec8b7e7f30cc4286de26c795f536d8c5e56
David C. Higler <davidhigler@gmail.com>
2021-12-23 21:12:43 +0100

    Module definitions and made objects for groups and rights

---
+++ src/Definition/Modules.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Definition;
+
+class Modules
+{
+    const MODULE_PORTFOLIO = 'portfolio';
+    const MODULE_INSPECTIONS = 'inspections';
+    const MODULE_REPORTS = 'reports';
+    const MODULE_MANAGEMENT = 'management';
+    const MODULE_ADMIN = 'admin';
+
+    const AVAILABLE = [
+        self::MODULE_PORTFOLIO,
+        self::MODULE_INSPECTIONS,
+        self::MODULE_REPORTS,
+        self::MODULE_MANAGEMENT,
+        self::MODULE_ADMIN
+    ];
+}
---
+++ src/Entity/Authorization/Group.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity\Authorization;
+
+use OpenApi\Annotations as OA;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\SuperClasses\Id;
+
+/**
+ * @author David C. Higler <davidhigler@gmail.com>
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="Groups")
+ *
+ * @OA\Schema()
+ */
+class Group extends Id
+{
+
+}
