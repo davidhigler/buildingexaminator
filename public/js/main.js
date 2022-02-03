06dd1406a457723da3678ef2b76739dd0873d5f1
David C. Higler <davidhigler@gmail.com>
2022-02-03 10:02:28 +0100

    Made the frontend communicate with the api

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,202 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('.modal').modal();
-    $('select').formSelect();
 });
+
+function showLoader() {
+    $( "div.overlay" ).show();
+}
+
+function hideLoader() {
+    $( "div.overlay" ).hide();
+}
+
+function loadHomePage() {
+    showLoader();
+    $('#slide-out').sidenav('close');
+    location.reload();
+}
+
+function loadHousingstocksPage() {
+    $.ajax({
+        url: "/api/buildingexaminator/v1/housingstocks",
+        type: "GET",
+        dataType: 'json',
+        accepts: {
+            json: 'application/json'
+        },
+        beforeSend: function () {
+            showLoader();
+            $('#slide-out').sidenav('close');
+        },
+        success: function (data) {
+            let rows = '';
+            $(data).each(function (index, element) {
+                rows += '            <tr>\n' +
+                    '                <td>' + element.code + '</td>\n' +
+                    '                <td>' + element.name + '</td>\n' +
+                    '                <td>\n' +
+                    '                    <button class="btn" name="edit" onclick="loadHousingstockEditPage(' + element.id + ');">Edit</button>\n' +
+                    '                    <button class="btn" name="delete" onclick="showDeleteHousingstockModal(' + element.id + ' , \'' + element.name + '\');">Delete</button>\n' +
+                    '                </td>\n' +
+                    '            </tr>\n';
+            });
+
+            $('div#content').html(
+                '    <h3 class="header">Housingstocks</h3>\n' +
+                '    <div class="row">\n' +
+                '        <div class="input-field col s6">\n' +
+                '            <button class="btn" name="new" onclick="loadHousingstockNewPage();">New</button>\n' +
+                '        </div>\n' +
+                '    </div>\n' +
+                '    <table>\n' +
+                '        <thead>\n' +
+                '            <tr>\n' +
+                '                <th>Code</th>\n' +
+                '                <th>Name</th>\n' +
+                '                <th>Action</th>\n' +
+                '            </tr>\n' +
+                '        </thead>\n' +
+                '        <tbody>\n' +
+                rows +
+                '        </tbody>\n' +
+                '    </table>\n'
+            );
+        },
+        error: function (jqXHR) {
+            loadErrorPage(jqXHR)
+        },
+        complete: function () {
+            hideLoader();
+        },
+    });
+}
+
+function loadHousingstockNewPage() {
+    showLoader();
+    $('#slide-out').sidenav('close');
+
+    $('div#content').html(
+        '    <h3 class="header">New housingstock</h3>\n' +
+        '    <form id="newhousingstock">\n' +
+        '        <div class="row">\n' +
+        '            <div class="input-field col s6">\n' +
+        '                <input id="code" type="text" class="validate">\n' +
+        '                <label for="code">Code</label>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="input-field col s6">\n' +
+        '                <input id="name" type="text" class="validate">\n' +
+        '                <label for="name">Name</label>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="input-field col s6">\n' +
+        '                <textarea id="description" class="materialize-textarea"></textarea>\n' +
+        '                <label for="description">Description</label>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="col s6">\n' +
+        '                <button type="submit" class="btn" name="create">Create</button>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </form>\n'
+    );
+
+    hideLoader();
+
+    $('form#newhousingstock').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: "/api/buildingexaminator/v1/housingstocks",
+            type: "POST",
+            dataType: 'json',
+            accepts: {
+                json: 'application/json'
+            },
+            data: JSON.stringify(
+                {
+                    'code': $('input#code').val(),
+                    'name': $('input#name').val(),
+                    'description': $('textarea#description').val(),
+                }
+            ),
+            beforeSend: function() {
+                showLoader();
+                $('#slide-out').sidenav('close');
+            },
+            success: function () {
+                loadHousingstocksPage();
+            },
+            error: function (jqXHR) {
+                loadErrorPage(jqXHR)
+            },
+            complete: function () {
+                hideLoader();
+            },
+        });
+    });
+}
+
+function loadHousingstockEditPage(id) {
+    console.log('Edit button pressed for housingstock ' + id + '.');
+}
+
+function showDeleteHousingstockModal(id, name) {
+    let modal = $('<div/>', {'id': 'modal-delete', 'class': 'modal'})
+        .append(
+            $('<div/>', {'class': 'modal-content'})
+                .append($('<h4/>').text('Delete ' + name))
+                .append($('<p/>').text('Are you sure?'))
+        )
+        .append(
+            $('<div/>', {'class': 'modal-footer'})
+                .append($('<a/>', {'class': 'modal-close btn-flat'}).text('No'))
+                .append($('<a/>', {'onclick': 'deleteHousingstock(' + id + ');', 'class': 'modal-close btn-flat'}).text('Yes'))
+        )
+        .appendTo('body')
+        .modal(
+            {
+                'dismissible': false,
+                'onCloseEnd': function () {
+                    $('div#' + this.id).remove();
+                    this.destroy();
+                }
+            }
+        );
+    let modalInstance = M.Modal.getInstance(modal);
+    modalInstance.open();
+}
+
+function deleteHousingstock(id) {
+    console.log('Delete button pressed and confirmed for housingstock ' + id + '.');
+}
+
+function loadErrorPage(jqXHR) {
+    console.log(jqXHR);
+
+    let response, message, IS_JSON = true;
+
+    try {
+        response = $.parseJSON(jqXHR.responseText);
+    } catch(err) {
+        IS_JSON = false;
+    }
+
+    if(IS_JSON) {
+        message = response.class + ': ' + response.detail;
+    } else {
+        message = jqXHR.status + ': ' + jqXHR.statusText;
+    }
+
+    $('div#content').html(
+        '    <h3 class="header">Error</h3>\n' +
+        '    <div class="card error red lighten-2">\n' +
+        '        <div class="card-content white-text valign-wrapper">\n' +
+        '            <i class="medium material-icons">error</i>\n' +
+        '            <p>' + message + '</p>\n' +
+        '        </div>\n' +
+        '    </div>\n'
+    );
+}
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,107 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\Portfolio\HousingStock;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
 
 class HomepageController extends AbstractController
 {
-    private array $controllerTwigVariables = [];
-
-    public function __construct(RouterInterface $router) {
-        $this->controllerTwigVariables['routes'] = [];
-        foreach ($router->getRouteCollection()->all() as $routeName => $routeObject) {
-            if (
-                str_starts_with($routeObject->getPath(), '/_') === false
-                && str_starts_with($routeObject->getPath(), '/api') === false
-            ) {
-                $this->controllerTwigVariables['routes'][$routeName] = [
-                    'methods' => join(', ', $routeObject->getMethods()),
-                    'title' => $routeObject->getOption('title'),
-                    'path' => $routeObject->getPath(),
-                ];
-            }
-        }
-    }
-
-    #[Route('/', name: 'homepage', options: ['title' => 'Home'], methods: ['GET'])]
-    public function Homepage(Request $request): Response
+    #[Route('/', name: 'homepage', methods: ['GET'])]
+    public function Homepage(): Response
     {
-        $this->addTitleToControllerTwigVariables($request);
         return $this->render(
             'index.twig',
-            $this->controllerTwigVariables,
+            [
+                'page' => [
+                    'title' => 'Dobro',
+                ],
+                'menuItems' => [
+                    'home' => [
+                        'title' => 'Home',
+                        'onclick' => 'loadHomePage();',
+                    ],
+                    'housingstocks' => [
+                        'title' => 'Housingstocks',
+                        'onclick' => 'loadHousingstocksPage();',
+                    ],
+                ],
+            ],
         );
-    }
-
-    #[Route('/housingstock', name: 'housingstock_overview', options: ['title' => 'Housingstocks'], methods: ['GET'])]
-    public function HousingStockOverview(Request $request): Response
-    {
-        $this->addTitleToControllerTwigVariables($request);
-        $housingStockRepository = $this->getDoctrine()->getRepository(HousingStock::class);
-        $housingstocks = $housingStockRepository->findAll();
-        return $this->render(
-            'api/v1/portfolio/housingstock/overview.twig',
-            array_merge(
-                $this->controllerTwigVariables,
-                [
-                    'housingstocks' => $housingstocks,
-                ]
-            ),
-        );
-    }
-
-    #[Route('/housingstock/new', name: 'housingstock_new', options: ['title' => 'New housingstock'], methods: ['GET'])]
-    public function HousingStockNew(Request $request): Response
-    {
-        $this->addTitleToControllerTwigVariables($request);
-
-        $housingstock = new HousingStock();
-        $housingstock->setCreationTime();
-        $housingstock->setLastChangeTime();
-
-        $form = $this->createFormBuilder($housingstock)
-            ->add('code', TextType::class, ['label' => 'Code', 'attr' => ['placeholder' => '123456']])
-            ->add('name', TextType::class, ['label' => 'Name', 'attr' => ['placeholder' => 'Main stock']])
-            ->add('description', TextareaType::class, ['label' => 'Description', 'attr' => ['placeholder' => 'This is the main stock of buildings we own']])
-            ->add('save', SubmitType::class, ['label' => 'Save'])
-            ->getForm();
-
-        return $this->render(
-            'api/v1/portfolio/housingstock/new_edit.twig',
-            array_merge(
-                $this->controllerTwigVariables,
-                [
-                    'form' => $form->createView(),
-                ]
-            ),
-        );
-    }
-
-    #[Route('/buildingaddress/new', name: 'buildingaddress_new', options: ['title' => 'New buildingaddress'], methods: ['GET'])]
-    public function BuildingAddressNew(Request $request): Response
-    {
-        $this->addTitleToControllerTwigVariables($request);
-        return $this->render(
-            'api/v1/portfolio/buildingaddress/new_edit.twig',
-            array_merge(
-                $this->controllerTwigVariables,
-                [
-                ]
-            ),
-        );
-    }
-
-    private function addTitleToControllerTwigVariables(Request $request)
-    {
-        $routeName = $request->attributes->get('_route');
-        $this->controllerTwigVariables['title'] = $this->controllerTwigVariables['routes'][$routeName]['title'];
     }
 }
