ae3170b559019980c8ec4fd19c13ce747ffc8e17
davidhigler <davidhigler@gmail.com>
2022-04-01 19:29:29 +0200

    Add edit page for building type

--- src/Security/ApiKeyAuthenticator.php
+++ src/Security/ApiKeyAuthenticator.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class ApiKeyAuthenticator extends AbstractAuthenticator
@@ -19,7 +21,10 @@ class ApiKeyAuthenticator extends AbstractAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        return new Passport(
+            new UserBadge('', ''),
+            new PasswordCredentials('')
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
