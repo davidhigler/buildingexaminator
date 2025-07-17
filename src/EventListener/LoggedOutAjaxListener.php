839ff5c500df88e3e83a0e343c8b2e08fc6219ad
davidhigler <davidhigler@gmail.com>
2025-07-17 11:39:40 +0200

    Adding an event listener to check logged in status on ajax calls.

---
+++ src/EventListener/LoggedOutAjaxListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class LoggedOutAjaxListener
+{
+    public function __construct(
+        private readonly Security $security,
+        private readonly UrlGeneratorInterface $urlGenerator,
+    ) {
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (($request->getAcceptableContentTypes()[0] ?? '') !== 'application/json') {
+            // Not an ajax request
+            return;
+        }
+
+        if ($event->getThrowable() instanceof AccessDeniedException === false) {
+            // Pass all other exceptions to the next exception listener
+            return;
+        }
+
+        if ($this->security->isGranted('ROLE_USER')) {
+            // The user is logged in already, the access denied exception is for something else
+            return;
+        }
+
+        // Redirect to log in screen when user is not logged in
+        $event->setResponse(
+            new RedirectResponse(
+                $this->urlGenerator->generate('logout')
+            )
+        );
+    }
+}
